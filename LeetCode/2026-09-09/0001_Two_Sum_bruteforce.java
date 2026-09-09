fa6405e6419313229a9a1931f770a00d5c00666d
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-09 22:40:11 +0530

    Day 01: Add Two Sum brute-force approach

---
+++ LeetCode/2026-09-09/0001_Two_Sum_bruteforce.java
@@ -0,0 +1,22 @@
+/*
+ * LeetCode #1 - Two Sum
+ * Brute-force approach learned on Day 01.
+ * Official problem: https://leetcode.com/problems/two-sum/
+ *
+ * Check every pair of elements.
+ * Time: O(n^2)
+ * Space: O(1) auxiliary space
+ */
+
+public class TwoSumBruteforce {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
