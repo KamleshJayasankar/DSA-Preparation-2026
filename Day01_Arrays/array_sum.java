333ed074d7f2c64dba26efcc63b9c6e11c8f6a69
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-09 22:39:13 +0530

    Day 01: Add standalone array sum practice

---
+++ Day01_Arrays/array_sum.java
@@ -0,0 +1,12 @@
+public class array_sum {
+    public static void main(String[] args) {
+        int[] nums = {5, 10, 15, 20, 25};
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
