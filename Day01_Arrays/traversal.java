380f8aba818455b1645bed56fc6a210eef083fac
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-09 22:39:09 +0530

    Day 01: Isolate array traversal practice

---
+++ Day01_Arrays/traversal.java
@@ -0,0 +1,9 @@
+public class traversal {
+    public static void main(String[] args) {
+        int[] nums = {5, 10, 15, 20, 25};
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
