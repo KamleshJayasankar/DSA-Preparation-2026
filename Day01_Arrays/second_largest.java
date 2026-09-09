146cf7b62d249c52bdcc9c4b9514eb0cb90e01f2
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-09 22:39:57 +0530

    Day 01: Correct second-largest handling for allowed duplicates

--- Day01_Arrays/second_largest.java
+++ Day01_Arrays/second_largest.java
@@ -6,10 +6,10 @@ public class second_largest {
         for (int i = 1; i < nums.length; i++) {
             int current = nums[i];
 
-            if (current > max1) {
+            if (current >= max1) {
                 max2 = max1;
                 max1 = current;
-            } else if (current > max2 && current < max1) {
+            } else if (current > max2) {
                 max2 = current;
             }
         }
