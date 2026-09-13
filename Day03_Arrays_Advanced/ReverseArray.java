0e9fea4104ef6d119bfee7a48ce606713bf145bc
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-13 07:54:14 +0530

    Day 03: Add in-place array reversal

---
+++ Day03_Arrays_Advanced/ReverseArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class ReverseArray {
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        reverse(nums, 0, n - 1);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
