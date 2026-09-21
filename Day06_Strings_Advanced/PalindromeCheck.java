44d9d95422e009bc1f364b7cfeddd94b16faa3da
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-21 14:18:29 +0530

    Day 06: Add PalindromeCheck.java

---
+++ Day06_Strings_Advanced/PalindromeCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    static boolean isPalindrome(String text) {
+        int left = 0, right = text.length() - 1;
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(isPalindrome(sc.nextLine()));
+    }
+}
