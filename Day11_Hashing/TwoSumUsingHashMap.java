d287e84b476a4b99e66181ed97421d360636b909
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-21 14:35:23 +0530

    Day 11: Add TwoSumUsingHashMap.java

---
+++ Day11_Hashing/TwoSumUsingHashMap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TwoSumUsingHashMap {
+    static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> seen = new HashMap<>();
+        for (int i=0;i<nums.length;i++) {
+            int needed=target-nums[i];
+            if(seen.containsKey(needed)) return new int[]{seen.get(needed),i};
+            seen.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+    public static void main(String[] args) { System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15},9))); }
+}
