9f3034f5c50f02a8d0c3e851dadc49e1ee870acd
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-21 14:35:20 +0530

    Day 11: Add HashSetDuplicateCheck.java

---
+++ Day11_Hashing/HashSetDuplicateCheck.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class HashSetDuplicateCheck {
+    static boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) if (!seen.add(num)) return true;
+        return false;
+    }
+    public static void main(String[] args) { System.out.println(containsDuplicate(new int[]{1,2,3,1})); }
+}
