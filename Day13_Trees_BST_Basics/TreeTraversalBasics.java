6c646cd7a54eece0aef7d72386885cd1f184ecb4
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-22 21:22:34 +0530

    Day 13: Add TreeTraversalBasics.java

---
+++ Day13_Trees_BST_Basics/TreeTraversalBasics.java
@@ -0,0 +1,37 @@
+public class TreeTraversalBasics {
+    static void preorder(TreeNode root) {
+        if (root == null) return;
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    static void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    static void postorder(TreeNode root) {
+        if (root == null) return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.print("Preorder: ");
+        preorder(root);
+        System.out.print("\nInorder: ");
+        inorder(root);
+        System.out.print("\nPostorder: ");
+        postorder(root);
+    }
+}
